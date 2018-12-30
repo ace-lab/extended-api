bea0bbb201949f57b33e3e336f70a0b6326a1b78
an-ju <an_ju@berkeley.edu>
2018-12-29 16:19:18 -0800

    Added an API to access historical snapshots.

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -46,37 +46,57 @@ class Snapshot < ApplicationRecord
       JSON.parse(resp.body, symbolize_names: true)
     end
 
+    # get tracker stories at a given time
+    def tracker_stories_at(project, time_at)
+      stories = tracker_stories project
+      activities = tracker_activities(project).select { |el| el[:occurred_at] > time_at }
+
+      # Reuse if previously queried
+      last_activity = activities.last
+      query_str = "extended_api/projects/#{project}/stories?before_id=#{last_activity[:guid]}"
+      snapshot = Snapshot.where(query: query_str)
+      return snapshot.last.content unless snapshot.empty?
+
+      reversed_stories = tracker_stories_reverse(stories, activities)
+      unless last_activity.nil?
+        create(origin: 'pivotal_tracker',
+               data_name: 'activities',
+               project_id: project,
+               query: query_str,
+               content: reversed_stories.to_json,
+               taken_at: time_at)
+      end
+
+      reversed_stories
+    end
+
     # reverse stories
-    def tracker_stories_reverse(stories, activities, project)
+    def tracker_stories_reverse(stories, activities)
       activities.each do |activity|
         if activity[:kind].eql? 'story_update_activity'
-          stories = reverse_story_update(stories, activity, project)
+          stories = reverse_story_update(stories, activity)
         elsif activity[:kind].eql? 'story_move_activity'
-          stories = reverse_story_move(stories, activity, project)
+          stories = reverse_story_move(stories, activity)
         elsif activity[:kind].eql? 'story_create_activity'
-          stories = reverse_story_create(stories, activity, project)
+          stories = reverse_story_create(stories, activity)
         elsif activity[:kind].eql? 'story_delete_activity'
-          stories = reverse_story_delete(stories, activity, project)
+          stories = reverse_story_delete(stories, activity)
         end
       end
+      stories
     end
 
     # reverse story_update_activity
-    def reverse_story_update(stories, action, project)
+    def reverse_story_update(stories, action)
       action[:changes].each do |change|
         story = stories.find { |s| s[:id].eql? change[:id] }
         story.update change[:original_values]
       end
-      # create(origin: 'pivotal_tracker',
-      #        data_name: 'stories',
-      #        project_id: project,
-      #        content: stories.to_json,
-      #        taken_at: action[:occurred_at])
       stories
     end
 
     # reverse story_move_activity
-    def reverse_story_move(stories, action, project)
+    def reverse_story_move(stories, action)
       action[:changes].each do |change|
         story = stories.delete_at(stories.find_index { |el| el[:id].eql? change[:id] })
         next_index = stories.find_index { |el| el[:id].eql? change[:original_values][:before_id] }
@@ -92,16 +112,11 @@ class Snapshot < ApplicationRecord
         end
         stories.insert(next_index, story)
       end
-      # create(origin: 'pivotal_tracker',
-      #        data_name: 'stories',
-      #        project_id: project,
-      #        content: stories.to_json,
-      #        taken_at: action[:occurred_at])
       stories
     end
 
     # reverse story_create_activity
-    def reverse_story_create(stories, action, project)
+    def reverse_story_create(stories, action)
       action[:changes].each do |change|
         stories = stories.delete_if { |el| el[:id].eql? change[:id] }
       end
@@ -109,7 +124,7 @@ class Snapshot < ApplicationRecord
     end
 
     # reverse story_delete_activity
-    def reverse_story_delete(stories, action, project)
+    def reverse_story_delete(stories, action)
       # It loses information!
       action[:primary_resources].each do |story|
         stories.push story
